8964671c8d9813de51b69855e46b33b16f9e4e3d
Ashfaaq-Damree <ashfaaq.damree@gws-technologies.com>
2024-06-26 14:19:03 +0400

    Update Model.php

--- inc/Model.php
+++ inc/Model.php
@@ -2,4 +2,20 @@
 
 class Model
 {
+    public static $post_type = '';
+
+    function __construct()
+    {
+        add_action('init', [$this, 'post_type_registration']);
+        add_action('init', [$this, 'taxonomy_registration']);
+    }
+
+
+    function post_type_registration()
+    {
+    }
+
+    function taxonomy_registration()
+    {
+    }
 }
